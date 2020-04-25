f52386be41edc38e1c7619afc7bbce1879b993e7
amjil <amjilt@aliyun.com>
2020-04-25 17:27:22 +0800

    block list

--- src/clj/clj_sms/services/blacklist.clj
+++ src/clj/clj_sms/services/blacklist.clj
@@ -2,31 +2,10 @@
   (:require
     [toucan.db :as db]
     [clj-sms.db.models :as models]
-    [clara.rules :refer [defrule defquery mk-session query insert insert! fire-rules]]
-    [clara.rules.accumulators :as acc]
     [clojure.tools.logging :as log]
     [clj-sms.config :refer [env]]))
 
-(defrecord Result [status msg])
-
-(defrecord Phone [num reason])
-
-(defrule add-block
-  [Phone (= ?num num) (= ?reason reason)]
-  =>
+(defn add-block [phone reason]
   (let [data (first (db/select models/Block :phone ?num))]
     (if data
-      (insert! (->Result false 'exists))
       (db/insert! models/Block :phone ?num :reason ?reason))))
-
-(defquery query-errors
-  [:?status]
-  [?errors <- Result (= ?status status)])
-
-(defn run-query [session]
-  (query session query-errors :?status false))
-
-(defn run-rules [phone reason]
-  (-> (mk-session 'clj-sms.services.blacklist)
-      (insert (->Phone phone reason))
-      (fire-rules)))
